12230afa1f37e5a9e9d50f7d77484d1a0028ae74
RiccardoK <webnoqs@gmail.com>
2017-10-30 15:09:17 +0100

    4.4  changed routes login etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+	as :user do
+	  get 'signin', to: 'devise/sessions#new', as: :new_user_session
+	  post 'signin', to: 'devise/sessions#create', as: :user_session
+	  match 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+	end
+	
   resources :users
   resources :products
     resources :orders, only: [:index, :show, :create, :destroy, :new, :edit ]
