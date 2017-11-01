bfd35b5baf504cb8699cc5dc6bea4595ec867464
RiccardoK <webnoqs@gmail.com>
2017-11-01 06:18:40 +0100

    5.7 adding comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,15 +90,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            'peter.testuro@gmail.com',
-  password:             'geheim01',
-  authentication:       :plain,
-  enable_starttls_auto: true
-}
+# config.action_mailer.smtp_settings = {
+#   address:              'smtp.gmail.com',
+#   port:                 587,
+#   domain:               'gmail.com',
+#   user_name:            'peter.testuro@gmail.com',
+#   password:             'geheim01',
+#   authentication:       :plain,
+#   enable_starttls_auto: true
+# }
 
 
 
