50e8cbfec01d0c3315f3cf1040fead78c456b135
RiccardoK <webnoqs@gmail.com>
2017-12-17 08:01:19 +0200

    6.12 redis installed

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,7 +18,9 @@ class Product < ApplicationRecord
     $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
   end
 
-
+def viewed!
+    $redis.incby("product:#{id}") # this is equivalent to 'INC product:1'
+  end
 
 
 def highest_rating_comment
