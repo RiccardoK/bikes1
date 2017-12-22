6d9e7118b8f94a079310eb0c932f9c8b63587b48
RiccardoK <webnoqs@gmail.com>
2017-12-22 08:41:22 +0100

    614 CRAZY VERSION

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -2,11 +2,9 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
   },
-
   disconnected: function() {
     // Called when the subscription has been terminated by the server
   },
-
   received: function(data) {
   // Called when there's incoming data on the websocket for this channel
   $(".alert.alert-info").show();
@@ -14,13 +12,12 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     $("#average-rating").attr('data-score', data.average_rating);
     refreshRating();
   },
-
 listen_to_comments: function() {
 return this.perform('listen', {
     product_id: $("[data-product-id]").data("product-id")
   });
 }
 });
-    $(document).on('turbolinks:load', function() {
-      App.product.listen_to_comments();
-      ); 
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
+});
