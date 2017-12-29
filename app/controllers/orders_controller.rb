5e516ed0b1f503d1c6d561a3be3da25328e007d7
RiccardoK <webnoqs@gmail.com>
2017-12-29 09:30:36 +0100

    6.15_fin_go

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,74 +1,38 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  #skip_before_filter  :verify_authenticity_token MAKES it vulnurable for attacks
+  protect_from_forgery with: :null_session
+  respond_to :json, :html
 
-  # GET /orders
-  # GET /orders.json
-   def index
-   @orders = Order.all
-   end
+  def index
+    @orders = Order.all.to_json(:include => [{:product => {:only => :name}}, {:user => {:only => :email}}])
+    respond_with @orders
+  end
 
-  # GET /orders/1
-  # GET /orders/1.json
   def show
+    @order = Order.find(params[:id]).to_json(:include => [{:product => {:only => :name}}, {:user => {:only => :email}}])
+    respond_with @order
   end
 
-  # GET /orders/new
   def new
-    @order = Order.new
   end
 
-  
-
-  # POST /orders
-  # POST /orders.json
   def create
-    @order = Order.new(order_params)
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: 'Oreder was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
+    @order = Order.create(order_params)
+    respond_with @order
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_with Order.update(params[:id], params[:order])
+  # end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
   def destroy
-    @order = Order.find(params[:id])
-    if @order.present?
-      @order.destroy
-    end
-    redirect_to root_url
-end
+    respond_with Order.destroy(params[:id])
+  end
 
+  private
 
-private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
+  def order_params
+    params.require(:order).permit(:product_id, :user_id, :total)
+  end
 
-    
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def order_params
-      params.require(:order).permit(:name, :description, :lastname, :user_id, :product_id )
-    end
 end
